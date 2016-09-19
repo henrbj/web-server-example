35d4a95af1ae8a46de527f441b883fb675c87710
henrbj <henrik_bjorkheim@hotmail.com>
2016-09-19 14:06:03 +0200

    Error in server_http solved

--- server_http.hpp
+++ server_http.hpp
@@ -355,9 +355,20 @@ namespace SimpleWeb {
                 auto response=std::shared_ptr<Response>(response_ptr);
                 send(response, [this, response, request, timer](const boost::system::error_code& ec) {
                     if(!ec) {
+                        
                         if(timeout_content>0)
                             timer->cancel();
-                        auto http_version=stof(request->http_version);
+                        
+                        float http_version;
+                        try{
+                            http_version=stof(request->http_version);
+                            
+                        }
+                        catch (const std::exception & e){
+                             if(exception_handler)
+                                 exception_handler(e);
+                          return;
+                          }
                         
                         auto range=request->header.equal_range("Connection");
                         for(auto it=range.first;it!=range.second;it++) {
